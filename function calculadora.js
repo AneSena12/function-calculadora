d0c4a4738269746f088d5ccc5a0227494b185c77
AneSena12 <140628944+AneSena12@users.noreply.github.com>
2023-07-28 13:19:13 -0300

    Update function calculadora.js

--- function calculadora.js
+++ function calculadora.js
@@ -1,12 +1,12 @@
 function calculadora (numero1, numero2, operacao) {
 
-if(operacao === "soma") {
+if(operacao === 1) {
     return numero1 + numero2
-} else if(operacao === "subtracao") {
+} else if(operacao === 2) {
     return numero1 - numero2
-} else if(operacao === "multiplicacao") {
+} else if(operacao === 3) {
     return numero1 * numero2
-} else if(operacao === "divisao") {
+} else if(operacao === 4) {
     return numero1 / numero2
 } else {
     return 0
@@ -16,7 +16,7 @@ if(operacao === "soma") {
 
 let numero1 = Number(prompt("Digite o primeiro número"))
 let numero2 = Number(prompt("Digite o segundo número"))
-let operacao = prompt("Digite a operação")
+let operacao = prompt("Digite o número referente a operação: 1. Soma, 2. Subtração, 3. Multiplicação, 4. Divisão:")
 const resultado = calculadora(numero1, numero2, operacao)
 
 console.log (resultado)
